89f9ef4f66863598fc769acf726d8463c4eb9e57
Jeremy <the.jeremy.cobb@gmail.com>
2024-01-07 17:19:14 -0600

    updated

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,50 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 
-require ("dotenv").config();
-
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/authenticate", async (req, res) => {
-    const { username } = req.body;
-    try {
-        const r = await axios.put(
-            "https://api.chatengine.io/users/",
-            { username: username, secret: username, first_name: username },
-            { headers: { "private-key": PRIVATE_KEY } }
-        );
-        return res.status(r.status).json(r.data)
-    } catch (error) {
-        return res.status(error.response.status).json(error.response.data);
-    }
+const CHAT_ENGINE_PROJECT_ID = process.env.PROJECT_KEY;
+const CHAT_ENGINE_PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+app.post("/signup", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+
+  // Store a user-copy on Chat Engine!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.post(
+      "https://api.chatengine.io/users/",
+      { username, secret, email, first_name, last_name },
+      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // Fetch this user from Chat Engine in this project!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+// vvv On port 3001!
 app.listen(3001);
